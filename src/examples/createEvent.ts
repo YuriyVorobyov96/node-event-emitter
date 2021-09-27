5b6d9f6deb4cee10862e7e4fb30745701037722a
YuriyVorobyov96 <darthrevan1333@gmail.com>
2021-09-27 08:53:48 +0300

    update first example

--- src/examples/createEvent.ts
+++ src/examples/createEvent.ts
@@ -1,23 +1,26 @@
+/* eslint-disable prefer-arrow-callback */
 /* eslint-disable no-magic-numbers */
 import MyEventEmitter from '../index';
 
 const myEmitter = new MyEventEmitter();
 
 // First listener
-myEmitter.on('event', () => {
+myEmitter.on('event', function firstListener() {
   console.log('Helloooo! first listener');
 });
 
 // Second listener
-myEmitter.on('event', (arg1, arg2) => {
+myEmitter.on('event', function secondListener(arg1, arg2) {
   console.log(`event with parameters ${arg1 as string}, ${arg2 as string} in second listener`);
 });
 
 // Third listener
-myEmitter.on('event', (...args) => {
+myEmitter.on('event', function thirdListener(...args) {
   const parameters = args.join(', ');
 
   console.log(`event with parameters ${parameters} in third listener`);
 });
+
+console.log(myEmitter.listeners('event'));
 
 myEmitter.emit('event', 1, 2, 3, 4, 5);
